33cddc9ea94cd42e6fe49d80271ad89580d75c84
4pcbr <me@4pcbr.com>
2016-04-20 21:35:16 +0000

    Added some missing dependencies

--- lib/omxplayer.ex
+++ lib/omxplayer.ex
@@ -1,2 +1,32 @@
 defmodule Omxplayer do
+  
+  use GenServer
+
+  def start_link do
+    { :ok, pid } = GenServer.start_link( __MODULE__, nil, name: __MODULE__ )
+  end
+
+  # Public API
+
+  def is_running? do
+    GenServer.call( __MODULE__, :is_running? )
+  end
+
+  def play( path, attrs ) do
+    GenServer.cast( __MODULE__, { :play, path, attrs } )
+  end
+
+  def stop, do: stop( false )
+  def stop( force ) do
+    GenServer.cast( __MODULE__, { :stop, force } )
+  end
+  # GenServer callbacks
+
+  # Helper functions
+
+  defp started_by do
+    #TODO
+  end
+
+
 end
--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Omxplayer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :omxplayer,
-     version: "0.0.1",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :omxplayer,
+      version: "0.0.1",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
   # Configuration for the OTP application
@@ -17,16 +19,9 @@ defmodule Omxplayer.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :dbus,        "~> 0.5.0"  },
+    ]
   end
 end
